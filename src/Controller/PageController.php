50589c84db14f95592566b2b497dd18ca41be9a5
KageHitokiri <skhcri@gmail.com>
2022-02-16 08:43:59 +0100

    Login & logout improvements

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -12,7 +12,7 @@ class PageController extends AbstractController
      * @Route("/", name="index")
      */
     public function index(): Response
-    {
+    {        
         return $this->render('page/index.html.twig', [
             'controller_name' => 'PageController',
         ]);
